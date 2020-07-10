831ac268c5c14052d6b151b57d3fdb3eef3414f9
Troydon Luicien <troyincarnate@gmail.com>
2020-07-10 23:36:00 +1000

    Fixed main nav styles.

--- GymPass/wwwroot/js/site.js
+++ GymPass/wwwroot/js/site.js
@@ -12,10 +12,12 @@ function closeNav() {
 }
 
 $(document).ready(function () {
-    $('div.sidenav a:nth-child(2), div.sidenav a:nth-child(3), div.sidenav a:nth-child(4)').mouseenter(function () {
+    //$('div.sidenav a:nth-child(1), div.sidenav a:nth-child(2), div.sidenav a:nth-child(3)').css("border", "1px solid grey");
+
+    $('.border-nav').mouseenter(function () {
         $(this).css("border", "1px solid blue");
     });
-    $('div.sidenav a:nth-child(2), div.sidenav a:nth-child(3), div.sidenav a:nth-child(4)').mouseleave(function () {
+    $('.border-nav').mouseleave(function () {
         $(this).css("border", "1px solid grey");
     });
 
