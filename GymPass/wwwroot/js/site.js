cc8867104d13b2f5dbc8633e9fcba5eb948b5ff1
Troydon Luicien <troyincarnate@gmail.com>
2020-07-10 21:48:13 +1000

    Completed desgin for the front-end.

--- GymPass/wwwroot/js/site.js
+++ GymPass/wwwroot/js/site.js
@@ -1,4 +1,22 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Navigation
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "80%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$(document).ready(function () {
+    $('div.sidenav a:nth-child(2), div.sidenav a:nth-child(3), div.sidenav a:nth-child(4)').mouseenter(function () {
+        $(this).css("border", "1px solid blue");
+    });
+    $('div.sidenav a:nth-child(2), div.sidenav a:nth-child(3), div.sidenav a:nth-child(4)').mouseleave(function () {
+        $(this).css("border", "1px solid grey");
+    });
+
+});
